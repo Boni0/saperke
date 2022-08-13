4bae3b625d9c397758b1347c7274865c2a5d05ad
Dawid Bonik <bonik.dawid@gmail.com>
2022-08-13 14:29:08 +0200

    fix/fix grid active state

--- src/ui/game_container/controllers.rs
+++ src/ui/game_container/controllers.rs
@@ -18,21 +18,21 @@ where
     ) {
         let hot = ctx.is_hot();
 
-        data.grid.is_active = if let GameState::EndState(_) = data.state {
-            false
-        } else {
-            hot && match event {
-                Event::MouseDown(mouse_event) | Event::MouseMove(mouse_event) => {
-                    (mouse_event.buttons.has_left()
+        match event {
+            Event::MouseDown(mouse_event) | Event::MouseMove(mouse_event) => {
+                data.grid.is_active = if let GameState::EndState(_) = data.state {
+                    false
+                } else {
+                    hot && (mouse_event.buttons.has_left()
                         && !mouse_event.buttons.has_middle()
                         && !mouse_event.buttons.has_right())
                         || (mouse_event.buttons.has_middle()
                             || (mouse_event.buttons.has_left() && mouse_event.buttons.has_right())
                             || (mouse_event.buttons.has_right() && mouse_event.buttons.has_left()))
-                }
-                _ => false,
+                };
             }
-        };
+            _ => {}
+        }
 
         child.event(ctx, event, data, env)
     }
